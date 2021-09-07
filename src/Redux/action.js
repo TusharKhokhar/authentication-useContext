47698013bc9d7d975a430b08cfee743dec933d73
TusharKhokhar <khokhartushar15@gmail.com>
2021-09-07 12:07:08 +0530

    first commit

---
+++ src/Redux/action.js
@@ -0,0 +1,17 @@
+export const userRequest = () => {
+    return {
+        type:'USER_REQUEST'
+    }
+}
+
+export const userSuccess = () => {
+    return {
+        type:'USER_SUCCESS'
+    }
+}
+
+export const userError = () => {
+    return {
+        type:'USER_ERROR'
+    }
+}
---
+++ src/routes/Routes.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useContext } from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import { AuthContext } from '../AuthContext/AuthContext'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Registration from '../pages/Registration'
+
+const Routes = (props) => {
+    const authCtx = useContext(AuthContext)
+    return (
+        <Switch>
+            <Route path='/' exact >
+                <Redirect to='/home' />
+            </Route>
+            <Route path='/home'>
+                <Home />
+            </Route>
+            {!authCtx.isLogin && <Route path='/login'>
+                <Login />
+            </Route>}
+            {!authCtx.isLogin && <Route path='/registration'>
+                <Registration />
+            </Route>}
+            <Route path='*' >
+                <Redirect to='/home' />
+            </Route>
+        </Switch>
+    )
+
+}
+
+export default Routes
